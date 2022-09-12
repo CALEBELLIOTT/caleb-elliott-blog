bd49813629bfefc1832787feec2743d0e49a745a
Caleb Elliott <caleb123elliott@gmail.com>
2022-09-12 17:10:25 -0600

    push to pull to laptop

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Portfolio from "./pages/Portfolio";
 import Blog from "./pages/Blog";
 import './index.css'
 import AboutPage from "./pages/About";
 import BlogPost from "./pages/BlogPost";
+import Portfolio from './pages/portfolio/Portfolio';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/pages/portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './PortfolioPage.css'
+
+export default function Portfolio() {
+  return (
+    <>
+      <div className="container-fluid portfolio-container">
+        <div className="row">
+          <h1>Caleb Elliott</h1>
+        </div>
+      </div>
+    </>
+  )
+}
