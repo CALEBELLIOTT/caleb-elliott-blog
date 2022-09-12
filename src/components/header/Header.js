ee45ce885e00332e26245dfc08d3a36d3c60201b
Caleb Elliott <caleb123elliott@gmail.com>
2022-09-12 17:43:39 -0600

    header responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -18,31 +18,56 @@ export class Header extends React.Component {
   render() {
     let categories = [{ name: 'Blog', route: '/blog' }, { name: 'Portfolio', route: '/portfolio' }, { name: 'About', route: '/about' }]
     return (
-      <div className="">
-        {/* <div className="banner-img d-flex align-items-center justify-content-end">
-          <img src="https://i0.wp.com/moneywithcaleb.com/wp-content/uploads/2021/11/logo-2.png?w=493&ssl=1" className="logo-img"></img>
-        </div> */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <div className="row nav-background">
-            <div className="col-12">
-              <div className="d-flex p-2 align-items-end justify-content-between">
-                <Link to={'/'}>
-                  <h1 className="mb-0">Caleb Elliott</h1>
-                </Link>
-                <div className="d-flex">
-                  {categories.map((c) => {
-                    return (
-                      <div>
+          <Link to={'/'}>
+            <h1 className="mb-0">Caleb Elliott</h1>
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {
+                categories.map(c => {
+                  return (
+                    <>
+                      <li className="nav-item">
                         <Link to={c.route} className="category-text pe-5 text-dark" key={c.name}>{c.name}</Link>
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
-            </div>
+                      </li>
+                    </>
+                  )
+                })
+              }
+            </ul>
           </div>
         </div>
-      </div>
+      </nav>
+      // <div className="">
+      //   {/* <div className="banner-img d-flex align-items-center justify-content-end">
+      //     <img src="https://i0.wp.com/moneywithcaleb.com/wp-content/uploads/2021/11/logo-2.png?w=493&ssl=1" className="logo-img"></img>
+      //   </div> */}
+      //   <div className="container-fluid">
+      //     <div className="row nav-background">
+      //       <div className="col-12">
+      //         <div className="d-flex p-2 align-items-end justify-content-between">
+      //           <Link to={'/'}>
+      //             <h1 className="mb-0">Caleb Elliott</h1>
+      //           </Link>
+      //           <div className="d-flex">
+      //             {categories.map((c) => {
+      //               return (
+      //                 <div>
+      //                   <Link to={c.route} className="category-text pe-5 text-dark" key={c.name}>{c.name}</Link>
+      //                 </div>
+      //               )
+      //             })}
+      //           </div>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </div>
     )
   }
 }
--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -6,7 +6,12 @@ export default function Portfolio() {
     <>
       <div className="container-fluid portfolio-container">
         <div className="row">
-          <h1>Caleb Elliott</h1>
+          <div className="col-12">
+            <div className="d-flex flex-column align-items-center mt-5">
+              <h1>Caleb Elliott</h1>
+              <h4 className="text-center">Passionate Full-Stack Software Developer</h4>
+            </div>
+          </div>
         </div>
       </div>
     </>
